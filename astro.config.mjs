52d0da611b30b6fb8d624a5258902ac91f90698a
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-05-30 18:13:49 +0200

    Revert "add NavigationBar component"
    This reverts commit 177a3fdfd9bee30d19a4d6c4b90ea866bb657531.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(), 
-  ],
-
+  integrations: [tailwind()]
 });
