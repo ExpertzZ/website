7e3c249ea904fbed1147b901ae0e6d571178e038
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-05-30 20:03:50 +0200

    Navigation Bar Design

---
+++ src/layout/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import '../styles/global.css';
+
+function NavigationBar () {
+  return (
+    <div className="flex justify-center h-20 items-center font-roboto font-black text-xl">
+        <ul className="flex gap-40">
+            <li className="text-indigo-400 ">PROJECTS</li>
+            <li className="text-indigo-400">CONTACT</li>
+            <li className="text-indigo-400">RESUME</li>
+        </ul>
+        
+    </div>
+  );
+}
+
+export default NavigationBar;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+			},
+
 			animation: {
 				tilt: 'tilt 10s infinite linear',
 			},
