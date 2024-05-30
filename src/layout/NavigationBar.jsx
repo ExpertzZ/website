9077e6a63d839715c424fda7db48b78f5a1f1986
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-05-30 20:37:20 +0200

    removed blankspace

--- src/layout/NavigationBar.jsx
+++ src/layout/NavigationBar.jsx
@@ -4,11 +4,10 @@ function NavigationBar () {
   return (
     <div className="flex justify-center h-20 items-center font-roboto font-black text-xl">
         <ul className="flex gap-40">
-            <li className="text-indigo-400 ">PROJECTS</li>
+            <li className="text-indigo-400">PROJECTS</li>
             <li className="text-indigo-400">CONTACT</li>
             <li className="text-indigo-400">RESUME</li>
         </ul>
-        
     </div>
   );
 }
