9f0fa289a5c7d4e477731f2fd83c8375d03ab3df
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-05-30 13:54:47 +0200

    extend tailwind config: animate-tilt

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+			animation: {
+				tilt: 'tilt 10s infinite linear',
+			},
+			keyframes: {
+				tilt: {
+					"0%, 50%, 100%": {
+						transform: "rotate(0deg)",
+					},
+					"25%": {
+						transform: "rotate(2deg)",
+					},
+					"75%": {
+						transform: "rotate(-2deg)",
+					},
+
+					}
+				}
+			}
+		},
 	plugins: [],
 }
