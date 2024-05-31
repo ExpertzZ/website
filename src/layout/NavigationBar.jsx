75a1d7e9e34756f9c83af2c135922d25e46d64ca
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-05-31 22:09:32 +0200

    Readme changes

--- src/layout/NavigationBar.jsx
+++ src/layout/NavigationBar.jsx
@@ -3,7 +3,7 @@ import '../styles/global.css';
 function NavigationBar () {
   return (
     <div className="flex justify-center h-20 items-center font-roboto font-black text-xl">
-        <ul className="flex gap-40">
+        <ul className="flex gap-36">
             <li className="text-indigo-400">PROJECTS</li>
             <li className="text-indigo-400">CONTACT</li>
             <li className="text-indigo-400">RESUME</li>
