fc6043a075687268b7407b516bc7975df9f0bf69
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-06-06 23:44:40 +0200

    position NavigationBar to the top
    It hovers over the Hero and its margin does not affect the Hero.

--- src/layout/NavigationBar.jsx
+++ src/layout/NavigationBar.jsx
@@ -2,7 +2,7 @@ import '../styles/global.css';
 
 function NavigationBar () {
   return (
-    <div className="flex justify-end h-20 items-center font-roboto font-black text-xl mr-96">
+    <div className="absolute top-0 right-0 flex justify-end h-20 items-center font-roboto font-black text-xl mr-96 z-10">
         <ul className="flex gap-36">
             <li className="text-indigo-400 hover:text-purple-600">PROJECTS</li>
             <li className="text-indigo-400 hover:text-purple-600">CONTACT</li>
